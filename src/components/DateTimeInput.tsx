a4681560ad642ba8f300171251f65f9183dc64dc
ammrysir <ammaryasir03062000@gmail.com>
2025-05-23 23:46:58 +0800

    [feat] refactor input styling

--- src/components/DateTimeInput.tsx
+++ src/components/DateTimeInput.tsx
@@ -7,12 +7,9 @@ export default function DateTimeInput() {
 	const [date, setDate] = createSignal("");
 
 	return (
-		<div class="modern-date-wrapper">
+		<div class="date-input-wrapper">
 			<label for="datepicker" class="date-label">Pilih Tarikh Mula</label>
-			<input
-				id="datepicker"
-				type="date"
-				class="date-input"
+			<input type="date" id="datepicker" class="date-input"
 				value={date()}
 				onInput={(e) => setDate(e.currentTarget.value)}
 			/>
