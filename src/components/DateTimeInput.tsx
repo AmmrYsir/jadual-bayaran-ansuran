013ff99dd108dc34999bf623ce56045fad245f56
ammrysir <ammaryasir03062000@gmail.com>
2025-05-23 22:51:04 +0800

    [feat] init DateTimeInput components

---
+++ src/components/DateTimeInput.tsx
@@ -0,0 +1,21 @@
+import type { Component } from 'solid-js';
+import './DateTimeInput.css';
+
+import { createSignal } from 'solid-js';
+
+export default function DateTimeInput() {
+	const [date, setDate] = createSignal("");
+
+	return (
+		<div class="modern-date-wrapper">
+			<label for="datepicker" class="date-label">Pilih Tarikh Mula</label>
+			<input
+				id="datepicker"
+				type="date"
+				class="date-input"
+				value={date()}
+				onInput={(e) => setDate(e.currentTarget.value)}
+			/>
+		</div>
+	);
+}
