325f53dfeb8455b91e6b22b1e0f4928fb860132e
ammrysir <ammaryasir03062000@gmail.com>
2025-05-23 22:31:58 +0800

    [feat]  update AppHeader

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,12 +1,26 @@
 import type { Component } from 'solid-js';
 import './AppHeader.css';
 
+// icon
+import { ChevronDownIcon, ChevronUpIcon } from 'lucide-solid';
+
+import { createSignal } from 'solid-js';
+
 const AppHeader: Component = () => {
-  return (
-    <div class="app-header">
-        
-    </div>
-  );
+	const [isOpen, setIsOpen] = createSignal(false);
+	const toggleDrawer = () => setIsOpen(!isOpen());
+	
+	return (
+		<div class={`app-header ${isOpen() ? "open" : ""}`}>
+			<div class="app-header-section">
+				<h2>Header Content</h2>
+				<p>This is the drawer content.</p>
+			</div>
+			<button class="toggle-button" onClick={toggleDrawer}>
+				{isOpen() ? <ChevronUpIcon />: <ChevronDownIcon/>}
+			</button>
+		</div>
+	);
 };
 
 export default AppHeader;
